0371f3f9bf74d54b593f51359bc8027b6ccf140c
Voltyvolt <in_my_love@hotmail.co.uk>
2022-02-20 00:30:01 +0700

    commit all

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => (
       <div className="footer">
         <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
           <Link to="/">
-            Cryptovolt Inc.
+            Metavolt Inc.
           </Link> <br />
           All Rights Reserved.
         </Typography.Title>
