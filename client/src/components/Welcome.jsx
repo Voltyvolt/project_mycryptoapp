a540eac35b400102b152ee8dadf6c6565076ba86
Voltyvolt <in_my_love@hotmail.co.uk>
2022-02-22 23:50:18 +0700

    Commit Add Router history for welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-sequences */
 /* eslint-disable no-constant-condition */
 import React, { useContext } from 'react';
+import { BrowserRouter as Router, useHistory } from 'react-router-dom';
 import { AiFillGitlab } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
@@ -22,6 +24,12 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
+  const history = useHistory();
+
+  const handleHistory = () => {
+    history.push('/welcome');
+  };
+
   const { connectWallet, connectedAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext);
 
   const handleSubmit = (e) => {
@@ -36,52 +44,54 @@ const Welcome = () => {
 
   return (
     <>
-      <Title level={1} className="heading"> การทำธุรกรรม </Title>
-      <Row gutter={[32, 32]} className="row">
-        <Col span={12} flex="1 0 25%">
-          <Form.Item>
-            <Image src="https://ethereum.org/static/4d030a46f561e5c754cabfc1a97528ff/3ba1a/impact_transparent.webp" />
-          </Form.Item>
-        </Col>
-        <Col span={12} flex="1 0 25%" className="blockchain-welcome" style={{ alignItems: 'center' }}>
-          <Form.Item>
-            <Title level={2} style={{ color: 'white', textAlign: 'center' }}> เชื่อมต่อธุรกรรมบน Blockchain</Title>
-            <Title level={5} style={{ color: 'white', textAlign: 'center', marginTop: '25px' }}><AiFillGitlab /> Your Address Book</Title>
+      <Router>
+        <Title level={1} className="heading"> การทำธุรกรรม </Title>
+        <Row gutter={[32, 32]} className="row">
+          <Col span={12} flex="1 0 25%">
+            <Form.Item>
+              <Image src="https://ethereum.org/static/4d030a46f561e5c754cabfc1a97528ff/3ba1a/impact_transparent.webp" />
+            </Form.Item>
+          </Col>
+          <Col span={12} flex="1 0 25%" className="blockchain-welcome" style={{ alignItems: 'center' }}>
+            <Form.Item>
+              <Title level={2} style={{ color: 'white', textAlign: 'center' }}> เชื่อมต่อธุรกรรมบน Blockchain</Title>
+              <Title level={5} style={{ color: 'white', textAlign: 'center', marginTop: '25px' }}><AiFillGitlab /> Your Address Book</Title>
 
-            <div className="eth-card white-glassmorphism">
-              <div className="container">
-                <SiEthereum fontSize={21} style={{ marginTop: '15px', marginLeft: '10px' }} />
-                <br />
-                <Title level={5} style={{ marginTop: '15px', marginLeft: '10px' }}>Address { shortenAddress(connectedAccount) }</Title>
+              <div className="eth-card white-glassmorphism">
+                <div className="container">
+                  <SiEthereum fontSize={21} style={{ marginTop: '15px', marginLeft: '10px' }} />
+                  <br />
+                  <Title level={5} style={{ marginTop: '15px', marginLeft: '10px' }}>Address { shortenAddress(connectedAccount) }</Title>
+                </div>
+                <div>
+                  <BsInfoCircle fontSize={20} style={{ marginTop: '12px', marginRight: '10px' }} />
+                </div>
               </div>
-              <div>
-                <BsInfoCircle fontSize={20} style={{ marginTop: '12px', marginRight: '10px' }} />
-              </div>
-            </div>
 
-            {!connectedAccount
+              {!connectedAccount
                   && (
-                  <Button type="primary" onClick={connectWallet} style={{ background: 'black', borderColor: 'white', marginTop: '25px' }} block>
+                  <Button type="primary" onClick={connectWallet, handleHistory} style={{ background: 'black', borderColor: 'white', marginTop: '25px' }} block>
                     Connect Wallet
                   </Button>
 )}
 
-            <Title level={5} style={{ color: 'white', textAlign: 'center', marginTop: '25px' }}><SiEthereum /> Address To</Title>
-            <Input placeholder="Address" name="addressTo" type="text" handleChange={handleChange} />
-            <Title level={5} style={{ color: 'white', textAlign: 'center', marginTop: '25px' }}><SiEthereum /> Amount (BNB)</Title>
-            <Input placeholder="Amount (BNB)" name="amount" type="number" handleChange={handleChange} />
-            <Title level={5} style={{ color: 'white', textAlign: 'center', marginTop: '25px' }}><SiEthereum /> Keyword</Title>
-            <Input placeholder="Keyword" name="keyword" type="text" handleChange={handleChange} />
-            <Title level={5} style={{ color: 'white', textAlign: 'center', marginTop: '25px' }}><SiEthereum /> Message</Title>
-            <Input placeholder="Message" name="message" type="text" handleChange={handleChange} />
-            {false
+              <Title level={5} style={{ color: 'white', textAlign: 'center', marginTop: '25px' }}><SiEthereum /> Address To</Title>
+              <Input placeholder="Address" name="addressTo" type="text" handleChange={handleChange} />
+              <Title level={5} style={{ color: 'white', textAlign: 'center', marginTop: '25px' }}><SiEthereum /> Amount (BNB)</Title>
+              <Input placeholder="Amount (BNB)" name="amount" type="number" handleChange={handleChange} />
+              <Title level={5} style={{ color: 'white', textAlign: 'center', marginTop: '25px' }}><SiEthereum /> Keyword</Title>
+              <Input placeholder="Keyword" name="keyword" type="text" handleChange={handleChange} />
+              <Title level={5} style={{ color: 'white', textAlign: 'center', marginTop: '25px' }}><SiEthereum /> Message</Title>
+              <Input placeholder="Message" name="message" type="text" handleChange={handleChange} />
+              {false
                 ? <Loader />
                 : (
                   <Button type="primary" size="large" style={{ marginTop: '25px' }} onClick={handleSubmit} block>Send Now</Button>
             )}
-          </Form.Item>
-        </Col>
-      </Row>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Router>
     </>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,35 +1,36 @@
+/* eslint-disable global-require */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  mode: "jit",
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      display: ["Open Sans", "sans-serif"],
-      body: ["Open Sans", "sans-serif"],
+      display: ['Open Sans', 'sans-serif'],
+      body: ['Open Sans', 'sans-serif'],
     },
     extend: {
       screens: {
-        mf: "990px",
+        mf: '990px',
       },
       keyframes: {
-        "slide-in": {
-          "0%": {
-            "-webkit-transform": "translateX(120%)",
-            transform: "translateX(120%)",
+        'slide-in': {
+          '0%': {
+            '-webkit-transform': 'translateX(120%)',
+            transform: 'translateX(120%)',
           },
-          "100%": {
-            "-webkit-transform": "translateX(0%)",
-            transform: "translateX(0%)",
+          '100%': {
+            '-webkit-transform': 'translateX(0%)',
+            transform: 'translateX(0%)',
           },
         },
       },
       animation: {
-        "slide-in": "slide-in 0.5s ease-out",
+        'slide-in': 'slide-in 0.5s ease-out',
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require('@tailwindcss/forms')],
 };
